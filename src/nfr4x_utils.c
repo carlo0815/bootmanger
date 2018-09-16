c8d9c369950a0080fb3e755145f95ba46e873613
toto <toto_golden@gmx.de>
2018-09-16 21:22:33 +0200

    Update nfr4x_utils.c

--- src/nfr4x_utils.c
+++ src/nfr4x_utils.c
@@ -150,7 +150,7 @@ int nfr4x_utils_find_and_mount()
 		nfr4x_utils_create_dir_tree();
 		
 		while ((dir = readdir(fd)) != NULL) {
-			if (strlen(dir->d_name) == 4 && memcmp(dir->d_name, "sd", 2) == 0) {
+			if (((strlen(dir->d_name) == 7 || strlen(dir->d_name) == 9) && memcmp(dir->d_name, "mmc", 3) == 0) || ((strlen(dir->d_name) == 3 || strlen(dir->d_name) == 4) && memcmp(dir->d_name, "sd", 2) == 0)) {
 				char device[255];
 				sprintf(device, "%s/%s", nfr4x_DEVICES_DIR, dir->d_name);
 				nfr4x_log(LOG_DEBUG, "%-33s: check device %s", __FUNCTION__, device);
@@ -199,7 +199,7 @@ nfr4x_device_item *nfr4x_utils_get_images()
 	fd = opendir(datadir);
 	if (fd) {
 		while ((dir = readdir(fd)) != NULL) {
-			if (((strlen(dir->d_name) == 7 || strlen(dir->d_name) == 9) && memcmp(dir->d_name, "mmc", 3) == 0) || ((strlen(dir->d_name) == 3 || strlen(dir->d_name) == 4) && memcmp(dir->d_name, "sd", 2) == 0)) {
+			if (strlen(dir->d_name) > 0 && dir->d_name[0] != '.') {
 				char base_dir[255];
 				sprintf(base_dir, "%s/%s", datadir, dir->d_name);
 
